bbf10ca6c0b03f45d667c42466b046f71eb2c924
ahyeongCho <jj132535@naver.com>
2024-11-04 22:39:20 +0900

    first commit for test.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,7 +4,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("홈")),
+      appBar: AppBar(title: Text("홈화면")),
       body: Center(
         child: Text("홈 화면"),
       ),
